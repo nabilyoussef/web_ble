2f29a7231ed6f6d50f05061144dfc91270590fce
Nabil Youssef <ynabil634@gmail.com>
2024-10-22 11:03:07 +0300

    up

--- gatt_server.py
+++ gatt_server.py
@@ -42,9 +42,6 @@ class Application(dbus.service.Object):
         self.services = []
         dbus.service.Object.__init__(self, bus, self.path)
         self.add_service(LetterService(bus, 0))
-       # self.add_service(HeartRateService(bus, 0))
-       # self.add_service(BatteryService(bus, 1))
-       # self.add_service(TestService(bus, 2))
 
     def get_path(self):
         return dbus.ObjectPath(self.path)
